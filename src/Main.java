dce642dd6ed76fdaa294d780844192e5ae0a7fe1
Luiz Augusto <luis2848@gmail.com>
2024-04-24 22:17:30 -0300

    teste de requisição

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String url_str = "https://v6.exchangerate-api.com/v6/ca744abd942dce7f81b60c01/latest/USD";
+
+// Making Request
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+// Convert to JSON
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject Objetojson = root.getAsJsonObject();
+
+// Accessing object
+        String req_result = Objetojson.get("result").getAsString();
+        System.out.println(Objetojson);
+//        System.out.println("Hello world!");
     }
 }
